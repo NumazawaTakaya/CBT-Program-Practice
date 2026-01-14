29497b55c082bb746f234068d8f99962e1820668
takayanumazawa <super.basket.natural.player557@gmail.com>
2026-01-14 17:03:15 +0900

    first commit

---
+++ CBT_Practice/Pages/CBT/SevenColumns/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CBT_Practice.Pages.CBT.SevenColumns
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
