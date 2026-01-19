911e001b46ce68a1bbe5d79b5efa05b607546cb1
takayanumazawa <super.basket.natural.player557@gmail.com>
2026-01-19 17:44:47 +0900

    1月19日の対応

---
+++ CBT_Practice/Pages/CBT/SevenColumns/Step04_CounterEvidence.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CBT_Practice.Pages.CBT.SevenColumns
+{
+    public class Step04_CounterEvidenceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
