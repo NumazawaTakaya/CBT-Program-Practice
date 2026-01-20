ed3890607b606a3bfee16a40698fbd8ed13491ed
takayanumazawa <super.basket.natural.player557@gmail.com>
2026-01-20 14:30:01 +0900

    1月20日の対応 フロント編

---
+++ CBT_Practice/Pages/CBT/SevenColumns/Step05_AdaptiveThoughts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CBT_Practice.Pages.CBT.SevenColumns
+{
+    public class Step05_AdaptiveThoughtsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CBT_Practice/Pages/CBT/SevenColumns/Step06_Result.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CBT_Practice.Pages.CBT.SevenColumns
+{
+    public class Step06_ResultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
